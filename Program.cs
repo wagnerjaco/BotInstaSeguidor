324fe518e5d24690fd28ea18d49cf410a871c093
wagnerjaco <wagnerjaco@gmail.com>
2023-11-23 14:17:41 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using prmToolkit.Selenium;
+using System;
+using OpenQA.Selenium.Chrome;
+
+namespace BotInstaSeguidor
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var driver = new ChromeDriver();
+            
+            driver.LoadPage(TimeSpan.FromSeconds(20), "https://www.google.com");
+
+
+        }
+    }
+}
