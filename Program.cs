e596939d3113edfd32405bbee5c9418f5aa0d2dc
wagnerjaco <wagnerjaco@gmail.com>
2023-11-23 14:49:13 -0300

    login no instagram

--- Program.cs
+++ Program.cs
@@ -10,7 +10,18 @@ namespace BotInstaSeguidor
         static void Main(string[] args) 
         {
             var driver = new ChromeDriver();
-            
+
+            try
+            {
+                driver.LoadPage(TimeSpan.FromSeconds(20), "https://www.google.com");
+                driver.SetText(By.Name("username"), "wagner.jaco");
+                driver.SetText(By.Name("password"), "131995Wa");
+            }
+            catch (Exception err)
+            {
+
+                Console.WriteLine(err.ToString());
+            }
             driver.LoadPage(TimeSpan.FromSeconds(20), "https://www.google.com");
 
 
