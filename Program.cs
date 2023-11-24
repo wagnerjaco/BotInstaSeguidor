6eb90ce270adc6f652abb3f6b55fa0bb85fc4ae1
wagnerjaco <wagnerjaco@gmail.com>
2023-11-23 22:40:20 -0300

    boto para seguir

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using prmToolkit.Selenium;
 using System;
 using OpenQA.Selenium.Chrome;
+using System.Configuration.Assemblies;
+using prmToolkit.Configuration;
+
 
 namespace BotInstaSeguidor
 {
@@ -10,19 +13,34 @@ namespace BotInstaSeguidor
         static void Main(string[] args) 
         {
             var driver = new ChromeDriver();
-
+            var passuser = Configuration.GetKeyAppSettings("senhainsta");
+            var username = Configuration.GetKeyAppSettings("usuarioinsta");
+            var perfilseg = Configuration.GetKeyAppSettings("pagusuario");
             try
             {
-                driver.LoadPage(TimeSpan.FromSeconds(20), "https://www.google.com");
-                driver.SetText(By.Name("username"), "wagner.jaco");
-                driver.SetText(By.Name("password"), "131995Wa");
+                driver.LoadPage(TimeSpan.FromSeconds(5), "https://www.instagram.com/");
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                driver.SetText(By.Name("username"), username);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                driver.SetText(By.Name("password"), passuser);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                driver.Submit(By.TagName("button"));
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+                driver.LoadPage(TimeSpan.FromSeconds(5), perfilseg );
+                driver.FindElement(By.XPath("//button[contains(text(), 'Seguir')]")).Click();
+
             }
             catch (Exception err)
             {
 
                 Console.WriteLine(err.ToString());
             }
-            driver.LoadPage(TimeSpan.FromSeconds(20), "https://www.google.com");
+            finally
+            {
+                driver.Close(); 
+                driver.Dispose();
+            }
+
 
 
         }
